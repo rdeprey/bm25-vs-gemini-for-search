2dbe140902120813da8dcc2559f2d7077e5ec4e4
Rebecca Deprey <453900+rdeprey@users.noreply.github.com>
2026-02-13 18:59:50 -0800

    fix type errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
-
 import "./globals.css";
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html>
       <body>{children}</body>
